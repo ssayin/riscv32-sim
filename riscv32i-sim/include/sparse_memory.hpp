d5ad13d0ede487a91654a2924d3660eaf35ffe47
Serdar Sayın <nefer.kha.ptah@gmail.com>
2022-10-12 14:46:50 +0300

    decl and define magic numbers in sparse_memory.hpp

--- riscv32i-sim/include/sparse_memory.hpp
+++ riscv32i-sim/include/sparse_memory.hpp
@@ -12,6 +12,9 @@ class sparse_memory {
   uint32_t ensure_page_exists(uint32_t addr);
 
 public:
+  constexpr static uint32_t page_size = 4096;
+  constexpr static uint32_t mask      = 0xFFFFF000;
+
   void     write_blocks(uint32_t virt_addr, void *ptr, int64_t size_in_bytes);
   uint8_t  read_byte(uint32_t off);
   uint16_t read_half(uint32_t off);
--- riscv32i-sim/src/sparse_memory.cpp
+++ riscv32i-sim/src/sparse_memory.cpp
@@ -3,9 +3,9 @@
 #include <iostream>
 
 uint32_t sparse_memory::ensure_page_exists(uint32_t addr) {
-  uint32_t key = addr & 0xFFFFF000;
+  uint32_t key = addr & mask;
   if (!page.contains(key))
-    page.emplace(key, std::make_unique<uint8_t[]>(4096));
+    page.emplace(key, std::make_unique<uint8_t[]>(page_size));
   return key;
 }
 
@@ -14,7 +14,7 @@ void sparse_memory::write_blocks(uint32_t virt_addr, void *ptr,
   while (size_in_bytes > 0) {
     uint32_t key      = ensure_page_exists(virt_addr);
     uint32_t off      = offset<0u, 11u>(virt_addr);
-    uint32_t page_end = 4096 - off;
+    uint32_t page_end = page_size - off;
     size_in_bytes -= page_end;
     write_block(&page[key].get()[offset<0u, 11u>(virt_addr)], ptr, page_end);
     virt_addr += page_end;
@@ -30,7 +30,7 @@ void *sparse_memory::write_block(uint8_t *page_offset, void *ptr,
 
 // TODO: add memory address decoder as a separate class.
 uint8_t sparse_memory::read_byte(uint32_t off) {
-  return page[off & 0xFFFFF000].get()[offset<0u, 11u>(off)];
+  return page[off & mask].get()[offset<0u, 11u>(off)];
 }
 
 // FIXME: reading at page boundary is A PROBLEM
@@ -44,10 +44,8 @@ uint32_t sparse_memory::read_word(uint32_t off) {
 }
 
 void sparse_memory::write_byte(uint32_t off, uint8_t b) {
-  uint32_t maskd = off & 0xFFFFF000;
-  if (!page.contains(maskd))
-    page.emplace(maskd, std::make_unique<uint8_t[]>(4096));
-  page[maskd].get()[offset<0u, 11u>(off)] = b;
+  uint32_t key                          = ensure_page_exists(off);
+  page[key].get()[offset<0u, 11u>(off)] = b;
 }
 
 void sparse_memory::write_half(uint32_t off, uint16_t h) {
