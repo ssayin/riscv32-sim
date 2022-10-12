206c224af6a9515d998fe302122d6d406fce3d14
Serdar Sayın <nefer.kha.ptah@gmail.com>
2022-10-12 10:52:38 +0300

    make building spike optional, it should not cause build to fail

--- cmake/riscv32i-sim.cmake
+++ cmake/riscv32i-sim.cmake
@@ -1,11 +1,19 @@
 set(CMAKE_SYSTEM_NAME riscv32i-sim)
 
-set(CMAKE_C_COMPILER riscv32-unknown-elf-gcc)
-set(CMAKE_CXX_COMPILER riscv32-unknown-elf-g++)
-
 set(CMAKE_FIND_ROOT_PATH /opt/riscv32)
 
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+find_program(FOUND_RISCV_GCC "riscv32-unknown-elf-gcc")
+find_program(FOUND_RISCV_CXX "riscv32-unknown-elf-g++")
+
+if(FOUND_RISCV_GCC)
+  set(CMAKE_C_COMPILER riscv32-unknown-elf-gcc)
+endif()
+
+
+if(FOUND_RISCV_CXX)
+  set(CMAKE_CXX_COMPILER riscv32-unknown-elf-g++)
+endif()
