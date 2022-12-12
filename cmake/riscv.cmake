ssayin/riscv32-sim

commit 2e1687738f6c218b490ada7b05373101bb0fe315

various changes
add program_counter class for debugging
dump ELF sections for debugging
fix sw syntax in test1.S

In the following object dump:

80000174:	00000297          	auipc	x5,0x0
80000178:	01428293          	add	x5,x5,20 # 80000188 <reset_vector+0x138>
8000017c:	34129073          	csrw	mepc,x5
80000180:	f1402573          	csrr	x10,mhartid
80000184:	30200073          	mret

mret sets PC to 8000018c, so I inserted a nop at 80000188

should I skip this mret which is not in a trap frame?
am I missing something?

riscv32-sim/include/iss_model.hpp
@@ -9,6 +9,7 @@
 #include "loader.hpp"
 #include "reg_file.hpp"
 #include "sparse_memory.hpp"
+#include "program_counter.hpp"
 
 #include <array>
 #include <cstdint>
@@ -18,9 +19,11 @@ class iss_model {
   reg_file      rf{};
   csr_file      cf;
 
+  bool _done = false;
+
   const uint32_t tohost_addr;
 
-  uint32_t PC{0};
+  program_counter PC;
   uint32_t alu_out{};
   uint32_t mem_out{};
 
@@ -45,7 +48,8 @@ class iss_model {
   void handle_sret();
 
 public:
-  int done() const { return mem.read_word(tohost_addr); }
+  bool done() const { return _done; }
+  uint32_t tohost() const { return mem.read_word(tohost_addr); }
   void step();
 
   iss_model(loader l, sparse_memory &mem);

riscv32-sim/include/loader.hpp
@@ -5,6 +5,7 @@
 #include <elfio/elfio.hpp>
 #include <string>
 #include <fmt/printf.h>
+#include <elfio/elfio_dump.hpp>
 
 class loader {
   ELFIO::elfio reader;
@@ -30,14 +31,35 @@ loader::loader(const std::string &file_name, MM &mem) {
   if (reader.segments.size() == 0)
     throw std::runtime_error("Loaded ELF has no segments");
 
+  ELFIO::dump::header( std::cout, reader );
+  ELFIO::dump::section_headers( std::cout, reader );
+  ELFIO::dump::segment_headers( std::cout, reader );
+  ELFIO::dump::symbol_tables( std::cout, reader );
+  ELFIO::dump::notes( std::cout, reader );
+  ELFIO::dump::modinfo( std::cout, reader );
+  ELFIO::dump::dynamic_tags( std::cout, reader );
+  ELFIO::dump::section_datas( std::cout, reader );
+  ELFIO::dump::segment_datas( std::cout, reader );
+
   std::for_each(reader.segments.begin(), reader.segments.end(),
                 [&mem](std::unique_ptr<ELFIO::segment> &s) {
                   if (s->get_type() == ELFIO::PT_LOAD) {
-                    fmt::print("ADDR: {:x}, FILE_SIZE: {:x}\n", s->get_virtual_address(), s->get_file_size());
                     mem.load(s->get_virtual_address(), (void *)s->get_data(),
                              s->get_file_size());
                   }
                 });
+
+  std::for_each(reader.sections.begin(), reader.sections.end(), [&](std::unique_ptr<ELFIO::section> &s) {
+    fmt::print("{} {}\n", s->get_name(), s->get_flags());
+  }
+ );
+
+  const ELFIO::section                *sec = reader.sections[".symtab"];
+  ELFIO::const_symbol_section_accessor symbols(reader, sec);
+
+  for (int i = 0; i < symbols.get_symbols_num(); ++i) {
+
+  }
 }
 
 #endif // RISCV32_SIM_LOADER_HPP

riscv32-sim/src/csr_file.cpp
@@ -21,7 +21,7 @@ void csr_file::write(uint32_t addr, uint32_t v) {
   if (is_readonly(addr) || (priv(addr) > to_int(mode)))
     throw sync_exception(trap_cause::exp_inst_illegal);
 
-  fmt::print("csr[{:#x}] <= {:#x}\t", addr, v); 
+  //fmt::print("csr[{:#x}] <= {:#x}\t", addr, v);
   csrs[addr] = v;
 }
 

riscv32-sim/src/iss_model.cpp
@@ -4,6 +4,20 @@
 #include <fmt/color.h>
 #include <fmt/format.h>
 
+iss_model::iss_model(loader l, sparse_memory &mem)
+    : mem(mem), tohost_addr{l.symbol("tohost")}, PC{l.entry()}, cf{mode} {
+
+  mem.write_word(tohost_addr, 0);
+  fmt::print("PC is set to {:x}\n", static_cast<uint32_t>(PC));
+  // Set read-only CSRs
+
+  // TODO: These CSRs may later be loaded during the boot process.
+  cf.write(to_int(csr::misa), misa_value);
+  cf.write(to_int(csr::sstatus), 0b1 << 8);
+  cf.write(to_int(csr::mstatus), 0b11 << 11);
+}
+
+
 void iss_model::trap_setup(trap_cause cause)
 {
   auto is_fatal = [](trap_cause cause) {
@@ -24,11 +38,11 @@ void iss_model::trap_setup(trap_cause cause)
   if (is_fatal(cause))
     throw std::runtime_error("exception is fatal");
 
-  cf.write(privilege_base | to_int(csr::uepc), PC + 4);
+  cf.write(privilege_base | to_int(csr::uepc), PC);
 
   auto tvec = cf.read(privilege_base | to_int(csr::utvec));
 
-  PC = (tvec & consts::tvec_base_addr_mask);
+  PC.set((tvec & consts::tvec_base_addr_mask));
 
   /*
      * syscall handler for testing
@@ -43,8 +57,8 @@ void iss_model::trap_setup(trap_cause cause)
                  "handling ecall, x17 is 93, x10 is {}. x2 is {}\n",
                  static_cast<int32_t>(rf.read(10)),static_cast<int32_t>(rf.read(2)));
 
-      // no gp, tohost=0 does not exit, now a0 <= 100000 trouble
-      mem.write_word(tohost_addr, rf.read(10) - 100000);
+      mem.write_word(tohost_addr, rf.read(10));
+      _done = true;
     }
     break;
   }
@@ -66,8 +80,9 @@ consts::sign_bit_mask; }; enum { direct   = 0, vectored = 1,
 }
 
 void iss_model::step() {
+  PC.set(PC + 4);
   uint32_t isn = mem.read_word(PC);
-  fmt::print("\n{:>#12x}\t", PC);
+  fmt::print("\n{:>#12x}\t", static_cast<uint32_t>(PC));
   fmt::print("{:>#12x}\t", isn);
   op dec = decode(isn);
 
@@ -96,7 +111,6 @@ void iss_model::step() {
     switch (dec.target) {
     case pipeline_target::csr:
       csr(dec);
-      PC = PC + 4;
       break;
     case pipeline_target::mem:
       mem_phase(dec);
@@ -105,6 +119,7 @@ void iss_model::step() {
       break;
     case pipeline_target::mret:
       handle_mret();
+      break;
     }
 
     wb_retire_phase(dec);
@@ -116,6 +131,8 @@ void iss_model::step() {
     fmt::print(fg(fmt::color{0xCFDBD5}), " {} ", ex.what());
     trap_setup(ex.cause());
   }
+
+  PC.update();
 }
 
 void iss_model::exec(op &dec) {
@@ -266,19 +283,20 @@ void iss_model::mem_phase(op &dec) {
     mem.write_word(alu_out, rf.read(dec.rs2));
     break;
   }
+
+  _done = (alu_out == tohost_addr);
 }
 
 void iss_model::wb_retire_phase(op &dec) {
   switch (dec.target) {
   case pipeline_target::mem:
     wb_retire_ls(dec);
-    PC += 4;
     break;
   case pipeline_target::alu:
     wb_retire_alu(dec);
     break;
   case pipeline_target::branch:
-    PC = PC + (alu_out ? dec.imm : 4);
+    PC.set(PC + (alu_out ? dec.imm : 4));
     break;
   default:
     break;
@@ -305,11 +323,10 @@ void iss_model::wb_retire_alu(op &dec) {
   case alu_type::_jal:
   case alu_type::_jalr:
     rf.write(dec.rd, PC + 4);
-    PC = alu_out;
+    PC.set(alu_out);
     break;
   default:
     rf.write(dec.rd, alu_out);
-    PC = PC + 4;
     break;
   }
 }
@@ -369,8 +386,10 @@ void iss_model::handle_mret() {
   mstat[consts::status_mpp]     = true;
   mstat[consts::status_mpp + 1] = true;
   cf.write(to_int(csr::mstatus), mstat.to_ulong());
-  PC   = cf.read(to_int(csr::mepc));
+  PC.set(cf.read(to_int(csr::mepc)) + 4);
   mode = mode_tmp;
+
+  //fmt::print("\tReturning to {:x}", static_cast<uint32_t>(PC));
 }
 
 void iss_model::handle_sret() {
@@ -381,17 +400,5 @@ void iss_model::handle_sret() {
   sstat[consts::status_spie] = false;
   sstat[consts::status_spp]  = false;
   cf.write(to_int(csr::sstatus), sstat.to_ulong());
-  PC = cf.read(to_int(csr::sepc));
-}
-
-iss_model::iss_model(loader l, sparse_memory &mem)
-    : mem(mem), tohost_addr{l.symbol("tohost")}, PC{l.entry()}, cf{mode} {
-
-  fmt::print("PC is set to {:x}\n", PC);
-  // Set read-only CSRs
-
-  // TODO: These CSRs may later be loaded during the boot process.
-  cf.write(to_int(csr::misa), misa_value);
-  cf.write(to_int(csr::sstatus), 0b1 << 8);
-  cf.write(to_int(csr::mstatus), 0b11 << 11);
+  PC.set(cf.read(to_int(csr::sepc)) + 4);
 }

riscv32-sim/include/program_counter.hpp
@@ -1,0 +1,24 @@
+#ifndef RISCV32_SIM_PROGRAM_COUNTER_HPP
+#define RISCV32_SIM_PROGRAM_COUNTER_HPP
+
+#include <cstdint>
+
+class program_counter {
+public:
+  program_counter(uint32_t x) : pc_next{x + 4}, pc{x} {}
+  void set(uint32_t x) { pc_next = x; }
+  void update() { pc = pc_next; }
+
+  program_counter(program_counter &&)            = delete;
+  program_counter(const program_counter &)       = delete;
+  program_counter &operator=(program_counter &&) = delete;
+  program_counter &operator=(program_counter &)  = delete;
+
+  operator uint32_t() { return pc; }
+
+private:
+  uint32_t pc_next = 0;
+  uint32_t pc      = 0;
+};
+
+#endif // RISCV32_SIM_PROGRAM_COUNTER_HPP

riscv32-sim/src/main.cpp
@@ -11,13 +11,12 @@ int main(int argc, char **argv) {
 
   sparse_memory mem;
   iss_model     model{loader(argv[1], mem), mem};
-  int stat;
 
-  while (!(stat = model.done())) {
+  while (!model.done()) {
     model.step();
   }
 
-  fmt::print("Exited with {}\n", stat + 100000);
+  fmt::print("Exited with {}\n", model.tohost());
 
   return 0;
 }

commit 4ad3e96b0d9fac00c4bec1979cb7966c2e9ad989

fix error caused PC = mepc + 4
this error is introduced in https://github.com/ssayin/riscv32-sim/commit/2e1687738f6c218b490ada7b05373101bb0fe315

according to the spec:
https://github.com/riscv/riscv-isa-manual/blob/master/src/machine.tex#L2717
https://github.com/riscv/riscv-isa-manual/blob/master/src/machine.tex#L2767

store PC + 4 in xepc register when entering a trap
PC = xepc when returning from a trap

cmake/riscv.cmake
@@ -1,37 +1,40 @@
-#include(CMakeForceCompiler)
+# include(CMakeForceCompiler)
 
-# usage
-# cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/rv32imac.cmake ../
+# usage cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/rv32imac.cmake ../
 
-# Look for GCC in path
-# https://xpack.github.io/riscv-none-embed-gcc/
-find_file(RISCV_XPACK_GCC_COMPILER_EXE "riscv-none-embed-gcc.exe" PATHS ENV INCLUDE)
+# Look for GCC in path https://xpack.github.io/riscv-none-embed-gcc/
+find_file(RISCV_XPACK_GCC_COMPILER_EXE "riscv-none-embed-gcc.exe" PATHS ENV
+                                                                        INCLUDE)
 find_file(RISCV_XPACK_GCC_COMPILER "riscv-none-embed-gcc" PATHS ENV INCLUDE)
 # New versions of xpack
-find_file(RISCV_XPACK_NEW_GCC_COMPILER_EXE "riscv-none-elf-gcc.exe" PATHS ENV INCLUDE)
+find_file(RISCV_XPACK_NEW_GCC_COMPILER_EXE "riscv-none-elf-gcc.exe"
+          PATHS ENV INCLUDE)
 find_file(RISCV_XPACK_NEW_GCC_COMPILER "riscv-none-elf-gcc" PATHS ENV INCLUDE)
 
-# Look for RISC-V github GCC
-# https://github.com/riscv/riscv-gnu-toolchain
-find_file(RISCV_XPACK_GCC_COMPILER_EXT "riscv32-unknown-elf-gcc.exe" PATHS ENV INCLUDE)
+# Look for RISC-V github GCC https://github.com/riscv/riscv-gnu-toolchain
+find_file(RISCV_XPACK_GCC_COMPILER_EXT "riscv32-unknown-elf-gcc.exe"
+          PATHS ENV INCLUDE)
 find_file(RISCV_XPACK_GCC_COMPILER "riscv32-unknown-elf-gcc" PATHS ENV INCLUDE)
 
 # Select which is found
-if (EXISTS ${RISCV_XPACK_NEW_GCC_COMPILER})
-    set(RISCV_GCC_COMPILER ${RISCV_XPACK_NEW_GCC_COMPILER})
-elseif (EXISTS ${RISCV_XPACK_GCC_NEW_COMPILER_EXE})
-    set(RISCV_GCC_COMPILER ${RISCV_XPACK_NEW_GCC_COMPILER_EXE})
-elseif (EXISTS ${RISCV_XPACK_GCC_COMPILER})
-    set(RISCV_GCC_COMPILER ${RISCV_XPACK_GCC_COMPILER})
-elseif (EXISTS ${RISCV_XPACK_GCC_COMPILER_EXE})
-    set(RISCV_GCC_COMPILER ${RISCV_XPACK_GCC_COMPILER_EXE})
-elseif (EXISTS ${RISCV_GITHUB_GCC_COMPILER})
-    set(RISCV_GCC_COMPILER ${RISCV_GITHUB_GCC_COMPILER})
-elseif (EXISTS ${RISCV_GITHUB_GCC_COMPILER_EXE})
-    set(RISCV_GCC_COMPILER ${RISCV_GITHUB_GCC_COMPILER_EXE})
-else ()
-    message(FATAL_ERROR "RISC-V GCC not found. ${RISCV_GITHUB_GCC_COMPILER} ${RISCV_XPACK_GCC_COMPILER} ${RISCV_GITHUB_GCC_COMPILER_EXE} ${RISCV_XPACK_GCC_COMPILER_EXE}")
-endif ()
+if(EXISTS ${RISCV_XPACK_NEW_GCC_COMPILER})
+  set(RISCV_GCC_COMPILER ${RISCV_XPACK_NEW_GCC_COMPILER})
+elseif(EXISTS ${RISCV_XPACK_GCC_NEW_COMPILER_EXE})
+  set(RISCV_GCC_COMPILER ${RISCV_XPACK_NEW_GCC_COMPILER_EXE})
+elseif(EXISTS ${RISCV_XPACK_GCC_COMPILER})
+  set(RISCV_GCC_COMPILER ${RISCV_XPACK_GCC_COMPILER})
+elseif(EXISTS ${RISCV_XPACK_GCC_COMPILER_EXE})
+  set(RISCV_GCC_COMPILER ${RISCV_XPACK_GCC_COMPILER_EXE})
+elseif(EXISTS ${RISCV_GITHUB_GCC_COMPILER})
+  set(RISCV_GCC_COMPILER ${RISCV_GITHUB_GCC_COMPILER})
+elseif(EXISTS ${RISCV_GITHUB_GCC_COMPILER_EXE})
+  set(RISCV_GCC_COMPILER ${RISCV_GITHUB_GCC_COMPILER_EXE})
+else()
+  message(
+    FATAL_ERROR
+      "RISC-V GCC not found. ${RISCV_GITHUB_GCC_COMPILER} ${RISCV_XPACK_GCC_COMPILER} ${RISCV_GITHUB_GCC_COMPILER_EXE} ${RISCV_XPACK_GCC_COMPILER_EXE}"
+  )
+endif()
 
 message("RISC-V GCC found: ${RISCV_GCC_COMPILER}")
 
@@ -51,24 +54,28 @@ set(CMAKE_SYSTEM_PROCESSOR rv32im_zicsr)
 set(CMAKE_EXECUTABLE_SUFFIX ".elf")
 
 # specify the cross compiler. We force the compiler so that CMake doesn't
-# attempt to build a simple test program as this will fail without us using
-# the -nostartfiles option on the command line
-#CMAKE_FORCE_C_COMPILER( "${RISCV_TOOLCHAIN_BIN_PATH}/${CROSS_COMPILE}gcc${RISCV_TOOLCHAIN_BIN_EXT}" GNU )
-#CMAKE_FORCE_CXX_COMPILER( "${RISCV_TOOLCHAIN_BIN_PATH}/${CROSS_COMPILE}g++${RISCV_TOOLCHAIN_BIN_EXT}" GNU )
+# attempt to build a simple test program as this will fail without us using the
+# -nostartfiles option on the command line CMAKE_FORCE_C_COMPILER(
+# "${RISCV_TOOLCHAIN_BIN_PATH}/${CROSS_COMPILE}gcc${RISCV_TOOLCHAIN_BIN_EXT}"
+# GNU ) CMAKE_FORCE_CXX_COMPILER(
+# "${RISCV_TOOLCHAIN_BIN_PATH}/${CROSS_COMPILE}g++${RISCV_TOOLCHAIN_BIN_EXT}"
+# GNU )
 set(CMAKE_ASM_COMPILER {CROSS_COMPILE}-gcc)
 set(CMAKE_AR ${CROSS_COMPILE}-ar)
 set(CMAKE_ASM_COMPILER ${CROSS_COMPILE}-gcc)
 set(CMAKE_C_COMPILER ${CROSS_COMPILE}-gcc)
 set(CMAKE_CXX_COMPILER ${CROSS_COMPILE}-g++)
 
-# We must set the OBJCOPY setting into cache so that it's available to the
-# whole project. Otherwise, this does not get set into the CACHE and therefore
-# the build doesn't know what the OBJCOPY filepath is
-set(CMAKE_OBJCOPY ${RISCV_TOOLCHAIN_BIN_PATH}/${CROSS_COMPILE}-objcopy
-        CACHE FILEPATH "The toolchain objcopy command " FORCE)
+# We must set the OBJCOPY setting into cache so that it's available to the whole
+# project. Otherwise, this does not get set into the CACHE and therefore the
+# build doesn't know what the OBJCOPY filepath is
+set(CMAKE_OBJCOPY
+    ${RISCV_TOOLCHAIN_BIN_PATH}/${CROSS_COMPILE}-objcopy
+    CACHE FILEPATH "The toolchain objcopy command " FORCE)
 
-set(CMAKE_OBJDUMP ${RISCV_TOOLCHAIN_BIN_PATH}/${CROSS_COMPILE}-objdump
-        CACHE FILEPATH "The toolchain objdump command " FORCE)
+set(CMAKE_OBJDUMP
+    ${RISCV_TOOLCHAIN_BIN_PATH}/${CROSS_COMPILE}-objdump
+    CACHE FILEPATH "The toolchain objdump command " FORCE)
 
 # Set the common build flags
 
@@ -76,7 +83,15 @@ set(CMAKE_OBJDUMP ${RISCV_TOOLCHAIN_BIN_PATH}/${CROSS_COMPILE}-objdump
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=${CMAKE_SYSTEM_PROCESSOR}")
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "")
-set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "")
-set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS}  -march=${CMAKE_SYSTEM_PROCESSOR}  -nostartfiles")
+set(CMAKE_C_FLAGS
+    "${CMAKE_C_FLAGS}"
+    CACHE STRING "")
+set(CMAKE_CXX_FLAGS
+    "${CMAKE_C_FLAGS}"
+    CACHE STRING "")
+set(CMAKE_ASM_FLAGS
+    "${CMAKE_C_FLAGS}"
+    CACHE STRING "")
+set(CMAKE_EXE_LINKER_FLAGS
+    "${CMAKE_EXE_LINKER_FLAGS}  -march=${CMAKE_SYSTEM_PROCESSOR}  -nostartfiles"
+)

cmake/riscv_test_env.cmake
@@ -1,4 +1,6 @@
 set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS}")
 set(RISCV_TEST_ENV_DIR "${PROJECT_SOURCE_DIR}/external/riscv-test-env")
 set(LINKER_SCRIPT "${RISCV_TEST_ENV_DIR}/p/link.ld")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -nostdlib -nodefaultlibs -fno-exceptions -T ${LINKER_SCRIPT}")
+set(CMAKE_EXE_LINKER_FLAGS
+    "${CMAKE_EXE_LINKER_FLAGS} -nostdlib -nodefaultlibs -fno-exceptions -T ${LINKER_SCRIPT}"
+)

cmake/submodule_update.cmake
@@ -10,16 +10,20 @@ if(GIT_FOUND AND EXISTS "${PROJECT_SOURCE_DIR}/.git")
     execute_process(
       COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
       WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-      RESULT_VARIABLE GIT_SUBMOD_RESULT
-    )
+      RESULT_VARIABLE GIT_SUBMOD_RESULT)
     if(NOT GIT_SUBMOD_RESULT EQUAL "0")
-      message(FATAL_ERROR "git submodule update --init --recursive failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
+      message(
+        FATAL_ERROR
+          "git submodule update --init --recursive failed with ${GIT_SUBMOD_RESULT}, please checkout submodules"
+      )
     endif()
   endif()
 endif()
 
-if(NOT EXISTS "${PROJECT_SOURCE_DIR}/external/ELFIO/CMakeLists.txt" OR NOT EXISTS "${PROJECT_SOURCE_DIR}/external/fmt/CMakeLists.txt")
-  message(FATAL_ERROR "The submodules were not downloaded! GIT_SUBMODULE was turned off or failed. Please update submodules and try again.")
+if(NOT EXISTS "${PROJECT_SOURCE_DIR}/external/ELFIO/CMakeLists.txt"
+   OR NOT EXISTS "${PROJECT_SOURCE_DIR}/external/fmt/CMakeLists.txt")
+  message(
+    FATAL_ERROR
+      "The submodules were not downloaded! GIT_SUBMODULE was turned off or failed. Please update submodules and try again."
+  )
 endif()
-
-

riscv32-sim/CMakeLists.txt
@@ -1,31 +1,29 @@
-set(
-        SRC_FILES
-        src/main.cpp
-        src/iss_model.cpp
-        src/decoder.cpp
-        src/sparse_memory.cpp
-        src/loader.cpp
-        include/common/csr.hpp
-        include/decoder.hpp
-        include/iss_model.hpp
-        include/loader.hpp
-        include/common/masks.hpp
-        include/common/misa.hpp
-        include/rv32_isn.hpp
-        include/sparse_memory.hpp
-        include/common/types.hpp
-        include/common/trap_cause.hpp
-        include/config.hpp
-        include/offset.hpp
-        src/reg_file.cpp
-        include/reg_file.hpp
-        include/common/consts.hpp
-        include/csr_file.hpp
-        src/csr_file.cpp
-        include/sync_exception.hpp
-        include/common/privilege_level.hpp
-        src/csr_file.cpp
-)
+set(SRC_FILES
+    src/main.cpp
+    src/iss_model.cpp
+    src/decoder.cpp
+    src/sparse_memory.cpp
+    src/loader.cpp
+    include/common/csr.hpp
+    include/decoder.hpp
+    include/iss_model.hpp
+    include/loader.hpp
+    include/common/masks.hpp
+    include/common/misa.hpp
+    include/rv32_isn.hpp
+    include/sparse_memory.hpp
+    include/common/types.hpp
+    include/common/trap_cause.hpp
+    include/config.hpp
+    include/offset.hpp
+    src/reg_file.cpp
+    include/reg_file.hpp
+    include/common/consts.hpp
+    include/csr_file.hpp
+    src/csr_file.cpp
+    include/sync_exception.hpp
+    include/common/privilege_level.hpp
+    src/csr_file.cpp)
 
 add_executable(riscv32-sim ${SRC_FILES})
 

riscv32-sim/include/common/types.hpp
@@ -15,7 +15,7 @@ concept Enum = std::is_enum<T>::value;
 
 template <typename T>
 concept Memory_Model = requires(T mm, uint32_t addr, void *ptr, int64_t size) {
-                         mm.load(addr, ptr, size);
-                       };
+  mm.load(addr, ptr, size);
+};
 
 #endif // RISCV32_SIM_TYPES_HPP

riscv32-sim/include/iss_model.hpp
@@ -1,34 +1,33 @@
 #ifndef RISCV32_SIM_ISS_MODEL_HPP
 #define RISCV32_SIM_ISS_MODEL_HPP
 
+#include "common/csr.hpp"
+#include "common/trap_cause.hpp"
 #include "config.hpp"
 #include "csr_file.hpp"
 #include "decoder.hpp"
-#include "common/csr.hpp"
-#include "common/trap_cause.hpp"
 #include "loader.hpp"
+#include "program_counter.hpp"
 #include "reg_file.hpp"
 #include "sparse_memory.hpp"
-#include "program_counter.hpp"
 
 #include <array>
 #include <cstdint>
 
 class iss_model {
-  sparse_memory& mem;
-  reg_file      rf{};
-  csr_file      cf;
+  sparse_memory &mem;
+  reg_file       rf{};
+  csr_file       cf;
 
   bool _done = false;
 
   const uint32_t tohost_addr;
 
   program_counter PC;
-  uint32_t alu_out{};
-  uint32_t mem_out{};
+  uint32_t        alu_out{};
+  uint32_t        mem_out{};
 
   privilege_level mode = privilege_level::machine;
-
 
   void exec(op &dec);
   void exec_alu(op &dec);
@@ -48,9 +47,9 @@ class iss_model {
   void handle_sret();
 
 public:
-  bool done() const { return _done; }
+  bool     done() const { return _done; }
   uint32_t tohost() const { return mem.read_word(tohost_addr); }
-  void step();
+  void     step();
 
   iss_model(loader l, sparse_memory &mem);
 };

riscv32-sim/include/loader.hpp
@@ -3,9 +3,9 @@
 
 #include "common/types.hpp"
 #include <elfio/elfio.hpp>
-#include <string>
-#include <fmt/printf.h>
 #include <elfio/elfio_dump.hpp>
+#include <fmt/printf.h>
+#include <string>
 
 class loader {
   ELFIO::elfio reader;
@@ -31,15 +31,15 @@ loader::loader(const std::string &file_name, MM &mem) {
   if (reader.segments.size() == 0)
     throw std::runtime_error("Loaded ELF has no segments");
 
-  ELFIO::dump::header( std::cout, reader );
-  ELFIO::dump::section_headers( std::cout, reader );
-  ELFIO::dump::segment_headers( std::cout, reader );
-  ELFIO::dump::symbol_tables( std::cout, reader );
-  ELFIO::dump::notes( std::cout, reader );
-  ELFIO::dump::modinfo( std::cout, reader );
-  ELFIO::dump::dynamic_tags( std::cout, reader );
-  ELFIO::dump::section_datas( std::cout, reader );
-  ELFIO::dump::segment_datas( std::cout, reader );
+  ELFIO::dump::header(std::cout, reader);
+  ELFIO::dump::section_headers(std::cout, reader);
+  ELFIO::dump::segment_headers(std::cout, reader);
+  ELFIO::dump::symbol_tables(std::cout, reader);
+  ELFIO::dump::notes(std::cout, reader);
+  ELFIO::dump::modinfo(std::cout, reader);
+  ELFIO::dump::dynamic_tags(std::cout, reader);
+  ELFIO::dump::section_datas(std::cout, reader);
+  ELFIO::dump::segment_datas(std::cout, reader);
 
   std::for_each(reader.segments.begin(), reader.segments.end(),
                 [&mem](std::unique_ptr<ELFIO::segment> &s) {
@@ -49,16 +49,15 @@ loader::loader(const std::string &file_name, MM &mem) {
                   }
                 });
 
-  std::for_each(reader.sections.begin(), reader.sections.end(), [&](std::unique_ptr<ELFIO::section> &s) {
-    fmt::print("{} {}\n", s->get_name(), s->get_flags());
-  }
- );
+  std::for_each(reader.sections.begin(), reader.sections.end(),
+                [&](std::unique_ptr<ELFIO::section> &s) {
+                  fmt::print("{} {}\n", s->get_name(), s->get_flags());
+                });
 
   const ELFIO::section                *sec = reader.sections[".symtab"];
   ELFIO::const_symbol_section_accessor symbols(reader, sec);
 
   for (int i = 0; i < symbols.get_symbols_num(); ++i) {
-
   }
 }
 

riscv32-sim/src/csr_file.cpp
@@ -21,7 +21,7 @@ void csr_file::write(uint32_t addr, uint32_t v) {
   if (is_readonly(addr) || (priv(addr) > to_int(mode)))
     throw sync_exception(trap_cause::exp_inst_illegal);
 
-  //fmt::print("csr[{:#x}] <= {:#x}\t", addr, v);
+  // fmt::print("csr[{:#x}] <= {:#x}\t", addr, v);
   csrs[addr] = v;
 }
 

riscv32-sim/src/iss_model.cpp
@@ -17,9 +17,7 @@ iss_model::iss_model(loader l, sparse_memory &mem)
   cf.write(to_int(csr::mstatus), 0b11 << 11);
 }
 
-
-void iss_model::trap_setup(trap_cause cause)
-{
+void iss_model::trap_setup(trap_cause cause) {
   auto is_fatal = [](trap_cause cause) {
     switch (cause) {
     case trap_cause::exp_inst_access_fault:
@@ -29,7 +27,7 @@ void iss_model::trap_setup(trap_cause cause)
     }
   };
 
-  uint16_t   privilege_base = to_int(mode) << 8;
+  uint16_t privilege_base = to_int(mode) << 8;
   assert((to_int(cause) & consts::sign_bit_mask) == 0);
 
   cf.write(privilege_base | to_int(csr::ucause), to_int(cause));
@@ -38,14 +36,15 @@ void iss_model::trap_setup(trap_cause cause)
   if (is_fatal(cause))
     throw std::runtime_error("exception is fatal");
 
-  cf.write(privilege_base | to_int(csr::uepc), PC);
+  cf.write(privilege_base | to_int(csr::uepc),
+           PC & consts::tvec_base_addr_mask);
 
   auto tvec = cf.read(privilege_base | to_int(csr::utvec));
 
-  PC.set((tvec & consts::tvec_base_addr_mask));
+  PC.set((tvec & consts::tvec_base_addr_mask) + 4);
 
   /*
-     * syscall handler for testing
+   * syscall handler for testing
    */
   switch (cause) {
   case trap_cause::exp_ecall_from_hs_mode:
@@ -55,7 +54,8 @@ void iss_model::trap_setup(trap_cause cause)
     if (rf.read(17) == 93) {
       fmt::print(fg(fmt::color{0xCFDBD5}),
                  "handling ecall, x17 is 93, x10 is {}. x2 is {}\n",
-                 static_cast<int32_t>(rf.read(10)),static_cast<int32_t>(rf.read(2)));
+                 static_cast<int32_t>(rf.read(10)),
+                 static_cast<int32_t>(rf.read(2)));
 
       mem.write_word(tohost_addr, rf.read(10));
       _done = true;
@@ -386,10 +386,10 @@ void iss_model::handle_mret() {
   mstat[consts::status_mpp]     = true;
   mstat[consts::status_mpp + 1] = true;
   cf.write(to_int(csr::mstatus), mstat.to_ulong());
-  PC.set(cf.read(to_int(csr::mepc)) + 4);
+  PC.set(cf.read(to_int(csr::mepc)));
   mode = mode_tmp;
 
-  //fmt::print("\tReturning to {:x}", static_cast<uint32_t>(PC));
+  // fmt::print("\tReturning to {:x}", static_cast<uint32_t>(PC));
 }
 
 void iss_model::handle_sret() {
@@ -400,5 +400,5 @@ void iss_model::handle_sret() {
   sstat[consts::status_spie] = false;
   sstat[consts::status_spp]  = false;
   cf.write(to_int(csr::sstatus), sstat.to_ulong());
-  PC.set(cf.read(to_int(csr::sepc)) + 4);
+  PC.set(cf.read(to_int(csr::sepc)));
 }

riscv32-sim/src/reg_file.cpp
@@ -1,6 +1,6 @@
 #include "reg_file.hpp"
-#include "fmt/core.h"
 #include "fmt/color.h"
+#include "fmt/core.h"
 #include <cassert>
 
 uint32_t reg_file::read(uint8_t index) {
@@ -16,8 +16,8 @@ void reg_file::write(uint8_t index, uint32_t data) {
   fmt::print("x");
   fmt::print(fg(fmt::color{0xE8EDDF}), "{}", index);
   fmt::print(" <= ");
-  fmt::print(fg(fmt::color{0xF5CB5C}),"{:#d}", static_cast<int32_t>(data));
-  fmt::print(fg(fmt::color{0xCFDBD5})," ({:#x})\t", data);
+  fmt::print(fg(fmt::color{0xF5CB5C}), "{:#d}", static_cast<int32_t>(data));
+  fmt::print(fg(fmt::color{0xCFDBD5}), " ({:#x})\t", data);
 
   x[index] = data;
 }

riscv32-sim/src/sparse_memory.cpp
@@ -28,7 +28,9 @@ void sparse_memory::load(uint32_t virt_addr, void *ptr, int64_t size_in_bytes) {
 
 uint8_t sparse_memory::read_byte(uint32_t off) {
   if (!page.contains(off & mask))
-    throw std::runtime_error(fmt::format("{} {} tried to read uninitialized memory {:x}", __FILE__, __LINE__, off));
+    throw std::runtime_error(
+        fmt::format("{} {} tried to read uninitialized memory {:x}", __FILE__,
+                    __LINE__, off));
   return page[off & mask].get()[offset<0u, 11u>(off)];
 }
 

test/gtest/CMakeLists.txt
@@ -1,12 +1,13 @@
 include(FetchContent)
 
 FetchContent_Declare(
-        googletest
-        URL https://github.com/google/googletest/archive/refs/tags/release-1.12.1.zip
-        DOWNLOAD_EXTRACT_TIMESTAMP true
-)
+  googletest
+  URL https://github.com/google/googletest/archive/refs/tags/release-1.12.1.zip
+      DOWNLOAD_EXTRACT_TIMESTAMP true)
 # For Windows: Prevent overriding the parent project's compiler/linker settings
-set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+set(gtest_force_shared_crt
+    ON
+    CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(googletest)
 
 enable_testing()
@@ -14,18 +15,13 @@ enable_testing()
 include(GoogleTest)
 
 macro(add_unit_test TEST)
-    add_executable(
-            ${TEST}
-            ${ARGN}
-    )
-    target_link_libraries(
-            ${TEST}
-            GTest::gtest_main
-            fmt::fmt
-    )
+  add_executable(${TEST} ${ARGN})
+  target_link_libraries(${TEST} GTest::gtest_main fmt::fmt)
 
-    target_include_directories(${TEST} PRIVATE ${PROJECT_SOURCE_DIR}/riscv32-sim/include)
-    gtest_discover_tests(${TEST})
+  target_include_directories(${TEST}
+                             PRIVATE ${PROJECT_SOURCE_DIR}/riscv32-sim/include)
+  gtest_discover_tests(${TEST})
 endmacro()
 
-add_unit_test(sparse_memory_test sparse_memory_test.cc ${PROJECT_SOURCE_DIR}/riscv32-sim/src/sparse_memory.cpp)
+add_unit_test(sparse_memory_test sparse_memory_test.cc
+              ${PROJECT_SOURCE_DIR}/riscv32-sim/src/sparse_memory.cpp)

test/isa/sim/CMakeLists.txt
@@ -2,24 +2,19 @@ include(riscv)
 
 # set(CMAKE_C_FLAGS "-Os -g -Wall -ffunction-sections")
 set(LINKER_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/serdar.ld")
-SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -nostdlib -nodefaultlibs -fno-exceptions -T ${LINKER_SCRIPT}")
-
+set(CMAKE_EXE_LINKER_FLAGS
+    "${CMAKE_EXE_LINKER_FLAGS} -nostdlib -nodefaultlibs -fno-exceptions -T ${LINKER_SCRIPT}"
+)
 
 add_library(serdar STATIC crt/crt.c)
 set_target_properties(serdar PROPERTIES LINK_DEPENDS "${LINKER_SCRIPT}")
 
-set(
-        SOURCES
-        example/shift.c
-        example/csr_exception.c
-        example/fib.c
-)
+set(SOURCES example/shift.c example/csr_exception.c example/fib.c)
 
-foreach (SRC ${SOURCES})
-    get_filename_component(TARGET ${SRC} NAME_WLE)
+foreach(SRC ${SOURCES})
+  get_filename_component(TARGET ${SRC} NAME_WLE)
 
-    add_executable(${TARGET} ${SRC})
-    set_target_properties(${TARGET} PROPERTIES LINK_DEPENDS "${LINKER_SCRIPT}")
-    target_link_libraries(${TARGET} PRIVATE serdar)
-endforeach ()
-
+  add_executable(${TARGET} ${SRC})
+  set_target_properties(${TARGET} PROPERTIES LINK_DEPENDS "${LINKER_SCRIPT}")
+  target_link_libraries(${TARGET} PRIVATE serdar)
+endforeach()

test/isa/tvm/CMakeLists.txt
@@ -1,15 +1,11 @@
 include(riscv)
 include(riscv_test_env)
 
-set(
-        SOURCES
-        test1.S
-)
+set(SOURCES test1.S)
 
-foreach (SRC ${SOURCES})
-    get_filename_component(TARGET ${SRC} NAME_WLE)
-    add_executable(${TARGET} ${SRC})
-    set_target_properties(${TARGET} PROPERTIES LINK_DEPENDS "${LINKER_SCRIPT}")
-    target_include_directories(${TARGET} PRIVATE "${RISCV_TEST_ENV_DIR}")
-endforeach ()
-
+foreach(SRC ${SOURCES})
+  get_filename_component(TARGET ${SRC} NAME_WLE)
+  add_executable(${TARGET} ${SRC})
+  set_target_properties(${TARGET} PROPERTIES LINK_DEPENDS "${LINKER_SCRIPT}")
+  target_include_directories(${TARGET} PRIVATE "${RISCV_TEST_ENV_DIR}")
+endforeach()