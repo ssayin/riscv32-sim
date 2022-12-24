6d5dae6716df16eb3eabbf77f77ef10361e171ec
Serdar Sayın <nefer.kha.ptah@gmail.com>
2022-12-25 02:13:59 +0300

    wrap the riscv-tests builddir locator with if-else statements

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-cmake -B build -DBUILD_TESTS=ON -GNinja
+cmake -B build -DBUILD_TESTS=ON -DISA_TESTS_DIR=/opt/riscv32 -GNinja
 ninja -C build
 ctest --test-dir build/test/ --output-on-failure
