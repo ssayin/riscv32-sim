df6de2558a24ba3346f1da46e2b95640864ed1d3
Serdar Sayın <serdarsayin@pm.me>
2023-01-04 21:31:50 +0300

    fix broken build: workflow gcc fails to deduce default template param

--- riscv32-sim/include/loader.hpp
+++ riscv32-sim/include/loader.hpp
@@ -11,14 +11,14 @@ class loader {
   ELFIO::elfio reader;
 
 public:
-  template <MemoryModel Model, bool Dump>
+  template <MemoryModel Model>
   explicit loader(const std::string &file_name, Model &mem);
 
   uint32_t symbol(const std::string &str);
   uint32_t entry() { return reader.get_entry(); }
 };
 
-template <MemoryModel Model, bool Dump = false>
+template <MemoryModel Model>
 loader::loader(const std::string &file_name, Model &mem) {
   if (!reader.load(file_name))
     throw std::runtime_error("File is not found or it is not an ELF file\n");
@@ -32,28 +32,28 @@ loader::loader(const std::string &file_name, Model &mem) {
   if (reader.segments.size() == 0)
     throw std::runtime_error("Loaded ELF has no segments");
 
-  if constexpr (Dump) {
-    ELFIO::dump::header(std::cout, reader);
-    ELFIO::dump::section_headers(std::cout, reader);
-    ELFIO::dump::segment_headers(std::cout, reader);
-    ELFIO::dump::symbol_tables(std::cout, reader);
-    ELFIO::dump::notes(std::cout, reader);
-    ELFIO::dump::modinfo(std::cout, reader);
-    ELFIO::dump::dynamic_tags(std::cout, reader);
-    ELFIO::dump::section_datas(std::cout, reader);
-    ELFIO::dump::segment_datas(std::cout, reader);
+  /* if constexpr (Dump) {
+     ELFIO::dump::header(std::cout, reader);
+     ELFIO::dump::section_headers(std::cout, reader);
+     ELFIO::dump::segment_headers(std::cout, reader);
+     ELFIO::dump::symbol_tables(std::cout, reader);
+     ELFIO::dump::notes(std::cout, reader);
+     ELFIO::dump::modinfo(std::cout, reader);
+     ELFIO::dump::dynamic_tags(std::cout, reader);
+     ELFIO::dump::section_datas(std::cout, reader);
+     ELFIO::dump::segment_datas(std::cout, reader);
 
-    std::for_each(reader.sections.begin(), reader.sections.end(),
-                  [&](std::unique_ptr<ELFIO::section> &s) {
-                    fmt::print("{} {}\n", s->get_name(), s->get_flags());
-                  });
+     std::for_each(reader.sections.begin(), reader.sections.end(),
+                   [&](std::unique_ptr<ELFIO::section> &s) {
+                     fmt::print("{} {}\n", s->get_name(), s->get_flags());
+                   });
 
-    const ELFIO::section                *sec = reader.sections[".symtab"];
-    ELFIO::const_symbol_section_accessor symbols(reader, sec);
+     const ELFIO::section                *sec = reader.sections[".symtab"];
+     ELFIO::const_symbol_section_accessor symbols(reader, sec);
 
-    for (int i = 0; i < symbols.get_symbols_num(); ++i) {
-    }
-  }
+     for (int i = 0; i < symbols.get_symbols_num(); ++i) {
+     }
+   } */
 
   std::for_each(reader.segments.begin(), reader.segments.end(),
                 [&mem](std::unique_ptr<ELFIO::segment> &s) {
