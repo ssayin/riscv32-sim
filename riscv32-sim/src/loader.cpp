3f03548becb623329d8363bd93db08a7e790cdbf
Serdar Sayın <serdarsayin@pm.me>
2023-01-26 18:28:01 +0300

    move loader ctor out-of-line

--- riscv32-sim/src/loader.cpp
+++ riscv32-sim/src/loader.cpp
@@ -3,6 +3,45 @@
 #include <elfio/elfio.hpp>
 #include <elfio/elfio_section.hpp>
 
+loader::loader(const std::string &file_name, sparse_memory_accessor &mem) {
+  /*
+   * Irrecoverable
+   * Can resort to exceptions if necessary in the future.
+   */
+
+  if (!reader.load(file_name)) {
+    fmt::print(stderr, "File is not an ELF file");
+    std::exit(EXIT_FAILURE);
+  }
+
+  if (reader.get_class() != ELFIO::ELFCLASS32) {
+    fmt::print(stderr, "Only 32-bit binaries are supported");
+    std::exit(EXIT_FAILURE);
+  }
+
+  if (reader.get_machine() != ELFIO::EM_RISCV) {
+    // std::cerr << "Loaded ELF does not target RISC-V";
+    fmt::print(stderr, "Loaded ELF does not target RISC-V");
+    std::exit(EXIT_FAILURE);
+  }
+
+  if (reader.segments.size() == 0) {
+    fmt::print(stderr, "Loaded ELF has no segments");
+    std::exit(EXIT_FAILURE);
+  }
+
+  // dump();
+
+  std::for_each(reader.segments.begin(), reader.segments.end(),
+                [&mem](std::unique_ptr<ELFIO::segment> &s) {
+                  if (s->get_type() == ELFIO::PT_LOAD) {
+                    auto addr = s->get_virtual_address();
+                    auto size = s->get_file_size();
+                    mem.write(addr, (void *)s->get_data(), size);
+                  }
+                });
+}
+
 uint32_t loader::symbol(const std::string &str) {
   const ELFIO::section                *sec = reader.sections[".symtab"];
   ELFIO::const_symbol_section_accessor symbols(reader, sec);
