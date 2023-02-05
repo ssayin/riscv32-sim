69feb4fdf71ec679971c2752eccfcf5880293941
Serdar Sayın <serdarsayin@pm.me>
2023-02-05 18:21:09 +0300

    workflows: attempt to build on windows-latest

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 BUILD_DIR=build
 
-cmake -B $BUILD_DIR -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=ON -DBUILD_RUNTIME=ON -DCOVERAGE=ON -DISA_TESTS_DIR=/opt/riscv32 -GNinja
+cmake -B $BUILD_DIR -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=ON -DBUILD_RUNTIME=ON -DCOVERAGE=ON -DISA_TESTS_DIR=/opt/riscv32 -DENABLE_TCP=ON -GNinja
 ninja -v -C $BUILD_DIR
 ctest --test-dir $BUILD_DIR --output-on-failure
 gcovr -e external -e build 
