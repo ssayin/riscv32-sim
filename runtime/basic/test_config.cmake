5d4d2a74b324bdeee68a3f99b3c17011c01000a5
Serdar Sayın <serdarsayin@pm.me>
2023-04-10 03:42:03 +0300

    Refactor runtime/basic and update CMakeLists.txt
    - Moved the source files in the basic tests from the runtime/basic/ directory to the new runtime/basic/src/ directory.
    
    - Moved serdar.ld from runtime/basic/ to runtime/basic/crt/.
    
    - Created test_config.cmake under runtime/basic to store the names of the test sources and their expected exit codes.
    
    - Updated the CMakeLists.txt file to reflect these changes.

---
+++ runtime/basic/test_config.cmake
@@ -0,0 +1,7 @@
+list(APPEND src/shift.c 0x18 \(24\))
+list(APPEND src/csr_exception.c 0x0 \(0\))
+list(APPEND src/fib.c 0xD \(13\))
+list(APPEND src/mul.c 0x250E6 \(151782\))
+list(APPEND src/mulhsu.c 0xFFFFFFFF \(-1\))
+list(APPEND src/divu.c 0x1 \(1\))
+list(APPEND src/timer.c 0x0 \(0\))
